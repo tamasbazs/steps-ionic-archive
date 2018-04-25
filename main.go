962f70c143b4a377adcd7f789b097e44521b75ea
tamasbazs <38215181+tamasbazs@users.noreply.github.com>
2018-04-25 09:53:09 +0200

    Update main.go

--- main.go
+++ main.go
@@ -414,6 +414,7 @@ func main() {
 		// platform add
 		for _, platform := range platforms {
 			cmdArgs := []string{"ionic"}
+			var newCmdArgs []string
 			if ionicMajorVersion > 2 {
 				cmdArgs = append(cmdArgs, "cordova")
 			}
